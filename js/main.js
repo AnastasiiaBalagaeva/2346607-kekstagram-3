c02d63758e214827ff2b47437a6a82d55df3232b
AnastasiiaBalagaeva <abalagaevaa@mail.ru>
2023-03-30 00:28:15 +0300

    one

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-let object {
-  id:
-  url:
-  description:
-  likes:
-  comments:
-};
+function getRandomNumber (min, max) {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+
+  function getPhotoInfo() {
+    let photos = [];
+    for (let i = 1; i <= 25; i++) {
+
+      let info = {
+      id: i,
+      url: 'photos/${i}.jpg',
+      description: 'stunning',
+      likes: getRandomNumber(15, 200),
+      comments: getRandomNumber(0, 200),
+      }
+      photos.push(info);
+    }
+    return photos;
+}
+getPhotoInfo();
+
