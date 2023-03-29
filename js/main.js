6810aeb240af0689d5845e4fc986ba12c33f71b2
AnastasiiaBalagaeva <abalagaevaa@mail.ru>
2023-03-29 21:16:21 +0300

    one

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-function getRandomNumber (min, max) {
-  if (min < 0 || max < 0) {
-    return -1;
-  }
-  return (Math.floor(Math.random() * (max - min + 1)) + min);
-  }
-
-
-function checkStringLength(str, maxLength) {
-  return str.length <= maxLength;
-}
+let object
